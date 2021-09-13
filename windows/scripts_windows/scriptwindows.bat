b5f3116353cebd7ad431b4fb9a867579dadb4721
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2021-09-13 20:06:07 +0200

    Update scriptwindows.bat

--- windows/scripts_windows/scriptwindows.bat
+++ windows/scripts_windows/scriptwindows.bat
@@ -1,7 +1,7 @@
 SET /a num=%random% %%100
 echo %num%
 IF %num% GTR 75 (
-	echo demo_Status;value=%num%;OK
+	echo "demo_Status;OK;value=%num%"
 ) ELSE	 (
-	echo demo_Status;value=%num%;CRITICAL
+	"echo demo_Status;value=%num%;CRITICAL"
 	)
