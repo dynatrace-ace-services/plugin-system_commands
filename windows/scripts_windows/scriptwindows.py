9720e7ccc8681d42563dc455068cc28c547f5529
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2021-09-13 23:22:07 +0200

    Add files via upload

--- windows/scripts_windows/scriptwindows.py
+++ windows/scripts_windows/scriptwindows.py
@@ -2,8 +2,8 @@ import random
 result=random.randint(0,9)
 #print(result)
 if result >  5:
-	print("demo_Status;OK;value="+str(result))
+	print("demo_Status;CRITICAL;value="+str(result))
 elif result > 2:
 	print("demo_Status;WARNING;value="+str(result))
 else:
-	print("demo_Status;CRITICAL;value="+str(result))
+	print("demo_Status;OK;value="+str(result))
